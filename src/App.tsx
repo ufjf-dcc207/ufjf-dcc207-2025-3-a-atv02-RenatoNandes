3e0b917fd7a9e04a5cba9bde56b795604a93ef15
Renato Nandes <renato.nandes@estudante.ufjf.br>
2025-09-23 22:12:54 -0300

    🚩 add do componente Disciplina e implementação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Disciplina from './disciplina.tsx'
+
 function App() {
   return (
   <>
     <h1>Hello World! 💡</h1>
     <h2>Hello React! ⚗️</h2>
+    <Disciplina />
   </>
   );
 }
---
+++ src/disciplina.tsx
@@ -0,0 +1,10 @@
+// arquivo src/Disciplina.tsx
+
+export default function Disciplina (){
+    return (
+        <>
+            <div>DCC207</div>
+            <div>Desenvolvimento Web Front-End</div>
+        </>
+    );
+}
